c3b8264562dfa58217869fe723b92d2210683977
Xero kun <elo48858@gmail.com>
2023-09-26 22:01:34 +0100

    fix(chore): Print errors to standard err instead

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args)
         .unwrap_or_else(|err| {
-            println!("Problem parsing arguments: {}", err);
+            eprintln!("Problem parsing arguments: {}", err);
             process::exit(1);
         });
 
     if let Err(err) = rust_grep::run(config) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
         process::exit(1);
     }
 }
