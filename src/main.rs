8a7a75a18faffe806ad53952636cf2bbdf314cc3
Xero kun <elo48858@gmail.com>
2023-09-26 19:08:23 +0100

    refactor: Extracted config logic to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::{env, fs};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let filepath = &args[2];
+
+    (query, filepath)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let (query, filepath) = parse_config(&args);
     let contents = fs::read_to_string(filepath)
         .expect("Should have been able to read the file");
 
