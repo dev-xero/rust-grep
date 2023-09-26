03ab09408b43c164a0036c8395aa1e298ade2ea0
Xero kun <elo48858@gmail.com>
2023-09-26 20:34:04 +0100

    refactor: Moved config and setup to lib.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,5 @@
-use std::{env, fs, process};
-use std::error::Error;
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments, expected format: [search_text] [file_path]");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config {
-            query,
-            file_path
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(&config.file_path)?;
-    println!("Contents: {}", contents);
-
-    Ok(())
-}
+use std::{env, process};
+use rust_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -40,7 +12,7 @@ fn main() {
     println!("Searching for: {}", config.query);
     println!("In file: {}\n", config.file_path);
 
-    if let Err(err) = run(config) {
+    if let Err(err) = rust_grep::run(config) {
         println!("Application error: {}", err);
         process::exit(1);
     }
