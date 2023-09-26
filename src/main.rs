255d38ead5ccbaf9730bee282870c8386396ea35
Xero kun <elo48858@gmail.com>
2023-09-26 20:15:34 +0100

    refactor: Improved error handling for config build

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 struct Config {
     query: String,
@@ -6,24 +6,28 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments, they should follow the format [search_text] [file_path]");
+            return Err("Not enough arguments, expected format: [search_text] [file_path]");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config {
+        Ok(Config {
             query,
             file_path
-        }
+        })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}", err);
+            process::exit(1);
+        });
     let contents = fs::read_to_string(&config.file_path)
         .expect("Should have been able to read the file");
 
