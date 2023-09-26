0f4746ce083b97af0e1093ede0bc9f4bf955d5ae
Xero kun <elo48858@gmail.com>
2023-09-26 19:58:04 +0100

    refactor: Extract config creation logic to separate method

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,21 @@ struct Config {
     file_path: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config {
-        query,
-        file_path
+        Config {
+            query,
+            file_path
+        }
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     let contents = fs::read_to_string(&config.file_path)
         .expect("Should have been able to read the file");
 
