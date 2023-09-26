c0758b1a0f5e778cc776ce44b3f18f25b0af8ef4
Xero kun <elo48858@gmail.com>
2023-09-26 21:55:36 +0100

    feat: Implemented optional case-insensitive searching

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, env};
 
 pub struct Config {
     pub query: String,
-    pub file_path: String
+    pub file_path: String,
+    pub ignore_case: bool
 }
 
 impl Config {
@@ -14,10 +15,12 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query,
-            file_path
+            file_path,
+            ignore_case
         })
     }
 }
@@ -25,7 +28,10 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case { search(&config.query, &contents) } 
+        else { search_case_insensitive(&config.query, &contents) };
+
+    for line in results {
         println!("{line}");
     }
 
