789003baebb6baef8f68cc95c8587d46a1f94743
Xero kun <elo48858@gmail.com>
2023-09-26 21:13:44 +0100

    test: Added testing code for search function

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.
+        ";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
