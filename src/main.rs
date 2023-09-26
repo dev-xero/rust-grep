c727da3b17391f8ada2c05a1381c7a1e25efc5bf
Xero kun <elo48858@gmail.com>
2023-09-26 18:52:03 +0100

    feat: Separated query and filepath from command-line args

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let query = &args[1];
+    let filepath = &args[2];
+
+    println!("Searching for: {}", query);
+    println!("In file: {}", filepath);
 }
