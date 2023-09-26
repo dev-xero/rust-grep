405e3feb37727196b7244b2edb0cbd544afc584b
Xero kun <elo48858@gmail.com>
2023-09-26 18:58:51 +0100

    feat: Implemented feature to read file contents to a string

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let filepath = &args[2];
+    let contents = fs::read_to_string(filepath)
+        .expect("Should have been able to read the file");
 
     println!("Searching for: {}", query);
     println!("In file: {}", filepath);
+    println!("Contents: {}", contents);
 }
