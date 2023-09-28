feae0c3d8a1b898e5a7e824ac10644d95dacbf64
Xero kun <elo48858@gmail.com>
2023-09-28 13:57:11 +0100

    refactor: Improved search function

--- src/lib.rs
+++ src/lib.rs
@@ -48,28 +48,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'b>(query: &str, contents: &'b str) -> Vec<&'b str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    let lower_query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(|line: &&'b str| line.to_lowercase().contains(&lower_query))
+        .collect()
 }
 
 #[cfg(test)]
